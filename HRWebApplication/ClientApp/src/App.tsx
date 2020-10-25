7a157caeefc8e1f703859a4b499522d679f72f21
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2020-10-25 18:51:10 +0700

    commit package.json is disturbed

--- HRWebApplication/ClientApp/src/App.tsx
+++ HRWebApplication/ClientApp/src/App.tsx
@@ -11,22 +11,26 @@ import DetailConfirmProducts from './pages/private/ConfirmProducts/DetailConfirm
 import MerchantList from './pages/private/Merchants/MerchantList';
 import MerchantDetail from './pages/private/Merchants/MerchantDetail';
 
+import ErrorBoundary from './components/ErrorBoundary';
+
 export default function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path={['/', '/login']} component={LoginPage} />
-        <Route exact path={'/admin'} component={OverviewPage} />
-        <Route exact path={'/admin/category'} component={CategoryPage} />
-        <Route exact path={'/admin/attributes'} component={AttributePage} />
-        <Route exact path={'/admin/set-attributes'} component={GroupSetAttributePage} />
-        <Route exact path={'/staff/confirm-products'} component={ConfirmProductsPage} />
-        <Route exact path={'/staff/confirm-products/:id'} component={DetailConfirmProducts} />
-        <Route exact path={'/staff/merchants'} component={MerchantList} />
-        <Route exact path={'/staff/merchant-detail/:id'} component={MerchantDetail} />
-        <Route exact path={'/staff/merchant-add'} component={MerchantDetail} />
-        <Route exact path={'*'} component={NotFoundPage} />
-      </Switch>
+      <ErrorBoundary>
+        <Switch>
+          <Route exact path={['/', '/login']} component={LoginPage} />
+          <Route exact path={'/admin'} component={OverviewPage} />
+          <Route exact path={'/admin/category'} component={CategoryPage} />
+          <Route exact path={'/admin/attributes'} component={AttributePage} />
+          <Route exact path={'/admin/set-attributes'} component={GroupSetAttributePage} />
+          <Route exact path={'/staff/confirm-products'} component={ConfirmProductsPage} />
+          <Route exact path={'/staff/confirm-products/:id'} component={DetailConfirmProducts} />
+          <Route exact path={'/staff/merchants'} component={MerchantList} />
+          <Route exact path={'/staff/merchant-detail/:id'} component={MerchantDetail} />
+          <Route exact path={'/staff/merchant-add'} component={MerchantDetail} />
+          <Route exact path={'*'} component={NotFoundPage} />
+        </Switch>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
--- HRWebApplication/ClientApp/src/pages/Login/index.tsx
+++ HRWebApplication/ClientApp/src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import * as Constant from '../../common/Constant';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Form, Input, Button, Checkbox, Tabs, Row, Col, Card, Divider } from 'antd';
-import LoginStyle from './Style';
+import Container from './style';
 import {
   UserOutlined,
   LockOutlined,
@@ -10,85 +10,77 @@ import {
   PhoneOutlined,
 } from '@ant-design/icons';
 
-class LoginPage extends Component {
-  login = () => {
-    // @ts-ignore
-    this.props.history.push(Constant.ROUTER_URL.OVERVIEW_PAGE);
+export default function () {
+  const history = useHistory();
+
+  const login = () => {
+    history.push('/admin');
   };
 
-  render() {
-    return (
-      <LoginStyle>
-        <Row justify="center">
-          <Col span={8} className="login-wrapper">
-            <Card className="login-card">
-              <Card.Meta
-                description={
-                  <Tabs type="card" style={{ minHeight: 290 }}>
-                    <Tabs.TabPane tab="Đăng nhập" key="1">
-                      <Form
-                        name="login"
-                        initialValues={{ remember: true }}
-                        // @ts-ignore
-                        onFinish={this.onFinish}
+  return (
+    <Container>
+      <Row justify="center">
+        <Col span={8} className="login-wrapper">
+          <Card className="login-card">
+            <Card.Meta
+              description={
+                <Tabs type="card" style={{ minHeight: 290 }}>
+                  <Tabs.TabPane tab="Đăng nhập" key="1">
+                    <Form name="login" initialValues={{ remember: true }}>
+                      <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Vui lòng nhập tên tài khoản' }]}
                       >
-                        <Form.Item
-                          name="username"
-                          rules={[{ required: true, message: 'Vui lòng nhập tên tài khoản' }]}
-                        >
-                          <Input prefix={<UserOutlined />} placeholder="Tên tài khoản" />
+                        <Input prefix={<UserOutlined />} placeholder="Tên tài khoản" />
+                      </Form.Item>
+                      <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Vui lòng nhập mật khẩu' }]}
+                      >
+                        <Input.Password
+                          prefix={<LockOutlined />}
+                          type="password"
+                          placeholder="Mật khẩu"
+                        />
+                      </Form.Item>
+                      <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                          <Checkbox>Nhớ đăng nhập</Checkbox>
                         </Form.Item>
-                        <Form.Item
-                          name="password"
-                          rules={[{ required: true, message: 'Vui lòng nhập mật khẩu' }]}
-                        >
-                          <Input.Password
-                            prefix={<LockOutlined />}
-                            type="password"
-                            placeholder="Mật khẩu"
-                          />
-                        </Form.Item>
-                        <Form.Item>
-                          <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Nhớ đăng nhập</Checkbox>
-                          </Form.Item>
-                        </Form.Item>
-                        <Form.Item>
-                          <Button block type="primary" size="large" onClick={this.login}>
-                            ĐĂNG NHẬP
-                          </Button>
-                        </Form.Item>
-                      </Form>
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="Quên mật khẩu" key="2">
-                      <b>Nhập email hoặc số điện thoại đăng ký để nhận mã</b> <br />
-                      <br />
-                      <Input
-                        addonBefore={<MailOutlined />}
-                        addonAfter={<SendOutlined onClick={() => console.log(1)} />}
-                      />
-                      <Divider>Hoặc</Divider>
-                      <Input
-                        addonBefore={<PhoneOutlined />}
-                        addonAfter={<SendOutlined onClick={() => console.log(1)} />}
-                      />
-                      <br />
-                      <br />
-                      <Input.Group compact>
-                        <Input addonBefore="PIN" style={{ width: '50%' }} />
-                        <Button type="primary">Tiếp tục</Button>
-                        <Button type="link">Gửi lại</Button>
-                      </Input.Group>
-                    </Tabs.TabPane>
-                  </Tabs>
-                }
-              />
-            </Card>
-          </Col>
-        </Row>
-      </LoginStyle>
-    );
-  }
+                      </Form.Item>
+                      <Form.Item>
+                        <Button block type="primary" size="large" onClick={login}>
+                          ĐĂNG NHẬP
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  </Tabs.TabPane>
+                  <Tabs.TabPane tab="Quên mật khẩu" key="2">
+                    <b>Nhập email hoặc số điện thoại đăng ký để nhận mã</b> <br />
+                    <br />
+                    <Input
+                      addonBefore={<MailOutlined />}
+                      addonAfter={<SendOutlined onClick={() => console.log(1)} />}
+                    />
+                    <Divider>Hoặc</Divider>
+                    <Input
+                      addonBefore={<PhoneOutlined />}
+                      addonAfter={<SendOutlined onClick={() => console.log(1)} />}
+                    />
+                    <br />
+                    <br />
+                    <Input.Group compact>
+                      <Input addonBefore="PIN" style={{ width: '50%' }} />
+                      <Button type="primary">Tiếp tục</Button>
+                      <Button type="link">Gửi lại</Button>
+                    </Input.Group>
+                  </Tabs.TabPane>
+                </Tabs>
+              }
+            />
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
-export default LoginPage;
