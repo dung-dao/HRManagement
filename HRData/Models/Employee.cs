fa4f654bc2087dd8c3732969ce54f0880663bbdf
Đào Mạnh Dũng <daomanhdung@hotmail.com>
2020-10-28 10:30:33 +0700

    Create employee models

---
+++ HRData/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRData.Models
+{
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+    public class Employee : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        public Sex Sex { get; set; }
+    }
+}
---
+++ HRData/Models/EmployeePosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRData.Models
+{
+    class EmployeePosition : EntityBase
+    {
+        public List<Employee> Employees { get; set; }
+        public List<Position> Positions { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
