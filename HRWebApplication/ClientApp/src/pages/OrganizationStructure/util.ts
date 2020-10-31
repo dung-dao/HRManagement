abf40de35d5282532b4084222de7f13c03a98955
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2020-10-31 11:12:20 +0700

    add tree-like table for the usecase organizational structure

--- HRWebApplication/ClientApp/src/pages/OrganizationStructure/util.ts
+++ HRWebApplication/ClientApp/src/pages/OrganizationStructure/util.ts
@@ -1,40 +1,49 @@
-// convert data from database to real data the frontend can use
-import { Api } from './mock-api';
+export interface ICircularData {
+  [key: string]: any;
+  children?: ICircularData[];
+}
 
-const api = new Api();
+interface IOptions {
+  level?: number; // -1 for infinite, 0 for root level only, 1 for 1 level
+  key?: string; // rowKey, default is 'id',
+}
 
-export const columns = [
-  {
-    title: 'Tên tổ chức',
-    dataIndex: 'organizationName',
-    key: 'organizationName',
-  },
-  {
-    title: 'Số thành viên',
-    dataIndex: 'numberOfMembers',
-    key: 'numberOfMembers',
-    width: '12%',
-  },
-  {
-    title: 'Trưởng tổ chức',
-    dataIndex: 'leader',
-    key: 'leader',
-    width: '30%',
-  },
-];
+// Thanks to this answer: https://github.com/ant-design/ant-design/issues/11615#issuecomment-455426294
+export function calculateAllExpandedRowKeys(data: ICircularData[] | undefined, options?: IOptions) {
+  const rowKeys: string[] = [];
+  if (!(data && data.length)) {
+    return rowKeys;
+  }
 
-export async function getDataTable() {
-  const convert = ({ id, name, status, leaderName, numberOfPeople, children }: any) =>
-    status === 'active'
-      ? {
-          key: id,
-          organizationName: name,
-          leader: leaderName,
-          numberOfMembers: numberOfPeople,
-          children: children?.length ?? children.map((it) => convert(it)),
+  const defaultOptions = {
+    level: -1,
+    key: 'id',
+    ...options,
+  };
+
+  const { level, key } = defaultOptions;
+
+  if (!data[0][key]) {
+    return rowKeys;
+  }
+
+  if (level === 0) {
+    return rowKeys;
+  }
+
+  function mapRowKeys(source: ICircularData[], currentLevel: number = 1) {
+    let keys: string[] = [];
+    source.forEach(({ children, ...rest }) => {
+      if (children) {
+        keys.push(rest[key]);
+        if (level < 0 || (level > 0 && level > currentLevel)) {
+          const childrenKeys = mapRowKeys(children, currentLevel + 1);
+          keys = keys.concat(childrenKeys);
         }
-      : undefined;
+      }
+    });
+    return keys;
+  }
 
-  const rawData = await api.getGlimpseOrganizationUnit();
-  return convert(rawData);
+  return mapRowKeys(data);
 }
