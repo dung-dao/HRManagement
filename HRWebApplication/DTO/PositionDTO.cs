c33aaa9428df9f54d48e1cb8cf9d8576ee1eddf8
Đào Mạnh Dũng <daomanhdung@hotmail.com>
2020-11-21 20:59:59 +0700

    Update PositionDTO

--- HRWebApplication/DTO/PositionDTO.cs
+++ HRWebApplication/DTO/PositionDTO.cs
@@ -1,17 +1,31 @@
-﻿using HRData.Models.JobModels;
+﻿using HRData.Models;
+using HRData.Models.JobModels;
 using System;
 
 namespace HRWebApplication.DTO
 {
     public class PositionDTO
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public Decimal Salery { get; set; }
+        //public DateTime StartDate { get; set; }
+        //public DateTime? EndDate { get; set; }
+        //public Decimal Salary { get; set; }
 
+        //public JobTitleDTO JobTitle { get; set; }
+        //public WorkTypeDTO WorkType { get; set; }
+        //public JobCategoryDTO JobCategory { get; set; }
+        //public OrganizationUnitDTO Unit { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Decimal Salary { get; set; }
+        #region Navigation
+        public EmployeeDTO Employee { get; set; }
         public JobTitleDTO JobTitle { get; set; }
-        public WorkTypeDTO EmploymentStatus { get; set; }
+        public WorkTypeDTO WorkType { get; set; }
         public JobCategoryDTO JobCategory { get; set; }
         public OrganizationUnitDTO Unit { get; set; }
+
+        public LeaveDetailDTO LeaveDetail { get; set; }
+        #endregion
     }
 }
