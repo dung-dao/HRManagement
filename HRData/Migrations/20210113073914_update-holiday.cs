f30416db1f65970b6b5496adbb670b38977d25d1
Đào Mạnh Dũng <daomanhdung@hotmail.com>
2021-01-13 14:39:58 +0700

    Add Description to TimeOffType

---
+++ HRData/Migrations/20210113073914_update-holiday.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HRData.Migrations
+{
+    public partial class updateholiday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "TimeOffTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "TimeOffTypes");
+        }
+    }
+}
